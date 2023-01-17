5706d631fcb388c512540dcea266eccf700d7b94
neu-002644413 <shi.chenghao@northeastern.edu>
2023-01-16 19:45:51 -0500

    assignment 0

--- FirstProject/src/Model/HelloWorld.java
+++ FirstProject/src/Model/HelloWorld.java
@@ -1,12 +1,14 @@
 package Model;
 
+import java.util.Arrays;
+
 public class HelloWorld {
 	int a = 5;
 	double b  = 2.0d;
 	float c = 3.0f;
 	String d = "Hello";
 	Boolean e = false;
-	int[] intArr = new int[5];
+	int[] intArr = new int[]{1,2,3,4,5};
 	
 	public static void main(String[] args) {
 		HelloWorld hw = new HelloWorld();
@@ -15,5 +17,6 @@ public class HelloWorld {
 		System.out.println("c: " + hw.c);
 		System.out.println("d: " + hw.d);
 		System.out.println("e: " + hw.e);
+		System.out.println("intArr: " + Arrays.toString(hw.intArr));
 	}
 }
